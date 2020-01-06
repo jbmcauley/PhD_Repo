d7841f77b2918f1ff539bbd5561afb8a777899e7
MCAULEY John <s1945757@ed.ac.uk>
2020-01-06 16:13:33 +0000

    Cri-Map

--- Scripts/CriMapCrying.R
+++ Scripts/CriMapCrying.R
@@ -2,5 +2,24 @@ rm(list=ls())
 
 library(devtools)
 install_github("susjoh/crimaptools")
-setwd("C:/Users/s1945757/crimaptools-master/data")
+setwd("C:/Users/s1945757/PhD_Repo/Cri_Map/crimaptools-master/crimaptools-master/")
 getwd()
+
+
+library(crimaptools)
+library(GenABEL)
+
+data(deer)
+create_crimap_input(gwaa.data = deer.abel, familyPedigree = deer.famped, analysisID = "3a", chr = 3, outdir = "crimap", clear.existing.analysisID = TRUE)
+
+run_crimap_prepare(genfile = "crimap/chr3a.gen")
+#Function has not produced the .loc .par and .dat files suggested in the tutorial
+dir("crimap")
+
+parse_mend_err(prefile = "crimap/chr3a.pre", genfile = "crimap/chr3a.gen", familyPedigree = deer.famped)
+
+run_crimap_map(genfile = "crimap/chr3a.gen") 
+dir("crimap")
+
+deer.map <- parse_map(mapfile = "crimap/chr3a.map") 
+head(deer.map)
