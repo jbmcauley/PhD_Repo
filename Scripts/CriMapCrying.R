33b94ac7f574b23196f346d9f9293ad9daca60c2
MCAULEY John <s1945757@ed.ac.uk>
2020-01-10 15:02:34 +0000

    update

--- Scripts/CriMapCrying.R
+++ Scripts/CriMapCrying.R
@@ -3,10 +3,9 @@
 rm(list=ls())
 
 library(devtools)
-install_github("susjoh/crimaptools")
+install_github("susjoh/crimaptools", force = TRUE)
 setwd("C:/Users/s1945757/PhD_Repo/Cri_Map/crimaptools-master/crimaptools-master/")
 getwd()
-
 #Creating Cri-map input files-----
 library(crimaptools)
 library(GenABEL)
@@ -14,7 +13,7 @@ library(GenABEL)
 data(deer)
 create_crimap_input(gwaa.data = deer.abel, familyPedigree = deer.famped, analysisID = "3a", chr = 3, outdir = "crimap", clear.existing.analysisID = TRUE)
 
-run_crimap_prepare(genfile = "crimap/chr3a.gen")
+run_crimap_prepare(genfile = "crimap/chr3a.gen", crimap.path = "C:/PathApps/crimap.exe")
 #Function has not produced the .loc .par and .dat files suggested in the tutorial
 #Using the terminal does work following the crimapinput1 preiously created (n,n,n,n,7,y,y) to generate .loc, .par, and .dat files... mention to Susan a potential issue in crimaptools
 #Alternatively the problem is due to miscommunication between R and Path, have had Path issues in past with Plink...
@@ -25,24 +24,25 @@ parse_mend_err(prefile = "crimap/chr3a.pre", genfile = "crimap/chr3a.gen", famil
 read.table("crimap/chr3a.mnd", header = T)
 #No Mendelian errors detected, in tutorial "2" are listed... Data already cleaned?
 #Next line only used if mendialian erros are present in order to mask them in the .gen file.
-#create_crimap_input (gwaa.data = deer.abel, 
-#                     familyPedigree = deer.famped, 
-#                     analysisID = "3a", 
-#                     chr = 3, 
-#                     outdir = "crimap", 
-#                     clear.existing.analysisID = TRUE, 
-#                     use.mnd = TRUE)
+create_crimap_input (gwaa.data = deer.abel, 
+                     familyPedigree = deer.famped, 
+                     analysisID = "3a", 
+                     chr = 3, 
+                     outdir = "crimap", 
+                     clear.existing.analysisID = TRUE, 
+                     use.mnd = TRUE)
 
 
 #If mendelian errors detected you will need to rerun the prepare function. Once again, might need to be done in terminal rather than crimap-tools
-run_crimap_prepare(genfile = "crimap/chr3a.gen") 
+run_crimap_prepare(genfile = "crimap/chr3a.gen", crimap.path = "C:/PathApps/crimap.exe") 
+parse_mend_err(prefile = "crimap/chr3a.pre", genfile = "crimap/chr3a.gen", familyPedigree = deer.famped)
 dir("crimap")
 #Repeat the process of looking for mendelian error until there are none.
 
 
 #Build a Linkage Map:----
 
-run_crimap_map(genfile = "crimap/chr3a.gen")
+run_crimap_map(genfile = "crimap/chr3a.gen", crimap.path = "C:/PathApps/crimap.exe")
 #.map file generated has size "0 B" This is a sign that something is not working in generation of the file. Will need to work with regular functions 
 #in terminal.
 dir("crimap")
@@ -52,8 +52,9 @@ deer.map <- parse_map(mapfile = "crimap/chr3a.map")
 
 #Characterizing recombination events:----
 
-run_crimap_chrompic(genfile = "crimap/chr3a.gen")
+run_crimap_chrompic(genfile = "crimap/chr3a.gen", crimap.path =  "C:/PathApps/crimap.exe")
 deer.cmpmap <- parse_map_chrompic(chrompicfile = "crimap/chr3a.cmp")
+head(deer.cmpmap)
 
 deer.xovers <- parse_crossovers(chrompicfile = "crimap/chr3a.cmp", familyPedigree = deer.famped)
 deer.xovers[1:2,]
@@ -69,6 +70,7 @@ physmap <- data.frame(SNP.Name = snpnames(deer.abel)[chromosome(deer.abel) == 3]
                       Order = 1:length(which(chromosome(deer.abel) == 3)), 
                       analysisID = "3a")
 deer.doubles <- check_double_crossovers(parsed.xovers = deer.xovers, physical.map = physmap)
+
 
 deer.remove <- subset(deer.doubles, Singleton == "yes")
 deer.xovers.clean <- revise_double_crossovers(parsed.xovers = deer.xovers, removesections = deer.remove)
---
+++ Scripts/Family_Variable_Build.R
@@ -0,0 +1,53 @@
+setwd("C:/Users/johnb/Downloads/crimaptools-master/crimaptools-master/data")
+library(crimaptools)
+load("deer.RData")
+library(reshape2)
+op.pair.zeros <- melt(deer.ped, id = "ANIMAL")
+
+op.pair.zeros <- op.pair.zeros[op.pair.zeros[,3] != 0,]
+row.names(op.pair.zeros) <- NULL
+
+GPa <- op.pair.zeros[1:14,]
+GMa <- op.pair.zeros[15:28,]
+
+
+#-----Grandfather Setup----
+PGrand <- list()
+counter <- 0
+for (i in 1:14) {
+  counter <- counter +1
+  PGrand[[counter]] <- GPa$value[which(GPa$ANIMAL ==GPa$value[i])]
+  }
+idx <- !(sapply(PGrand, length))
+PGrand[idx] <- 0
+PGrand <- do.call(rbind, PGrand)
+GMa$PGrand <- PGrand
+
+
+
+#-----GrandMother Setup----
+MGrand <- list()
+counter <- 0
+for (i in 1:14) {
+  counter <- counter + 1
+  MGrand[[counter]] <- GMa$value[which(GMa$ANIMAL == GMa$value[i])]
+}
+idx <- !(sapply(MGrand, length))
+MGrand[idx] <- 0
+MGrand <- do.call(rbind, MGrand)
+GMa$MGrand <- MGrand
+
+op.pair.zeros <- rbind(GPa, GMa)
+
+#-----Rebuild .ped-----
+deer.ped$MGrand <- MGrand
+deer.ped$PGrand <- PGrand
+
+
+#Long Format
+op.pair.zeros <- melt(deer.ped, id = "ANIMAL")
+op.pair.zeros <- op.pair.zeros[op.pair.zeros[,3] != 0,]
+row.names(op.pair.zeros) <- NULL
+
+#Wide Format
+op.pair <- dcast(op.pair.zeros, ANIMAL ~ variable, value.var = "value")
